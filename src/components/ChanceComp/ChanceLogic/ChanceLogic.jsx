a30e80346373ca9d63a0f698ab326870a487bfe8
Janine Carl <janine.supercode@gmail.com>
2024-07-19 01:29:42 +0200

    update fetching for chance

--- src/components/ChanceComp/ChanceLogic/ChanceLogic.jsx
+++ src/components/ChanceComp/ChanceLogic/ChanceLogic.jsx
@@ -1,25 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import "./ChanceLogic.scss";
 import movies from "../../../utils/movies.json";
+import loadData from '../../../utils/DataFetch';
+import { useParams } from 'react-router-dom';
+
 
 const ChanceLogic = () => {
-  const [suggestion, setSuggestion] = useState({});
+  const [suggestion, setSuggestion] = useState(null);
+  const [suggestionData, setSuggestionData] = useState(null);
+  const [error, setError] = useState(null);
+  const [trigger, setTrigger] = useState(false);
 
-  useEffect(()=>{
-    makeSuggestion();
-  },[]);
+  let { genre } = useParams();
 
-  const makeSuggestion = ()=>{
-    const randomMovie = Math.floor(Math.random() * movies.length);
-    const suggest = movies[randomMovie];
+  useEffect(() => {
+    if (movies[genre]) {
+      const randomMovie = Math.floor(Math.random() * movies[genre].length);
+      const suggest = movies[genre][randomMovie];
+      console.log("suggest: ", suggest);
+      console.log("randomMovie: ", randomMovie);
 
-    setSuggestion(suggest);
+      setSuggestion(suggest);
+    } else {
+      setError('Genre not found');
+    }
+  }, [genre, trigger]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (suggestion) {
+        console.log("Fetching data for suggestion:", suggestion);
+        const data = await loadData(suggestion);
+        if (data.error) {
+          setError(data.error);
+        } else {
+          console.log("Data fetched successfully:", data);
+          setSuggestionData(data);
+        }
+      }
+    };
+
+    fetchData();
+  }, [suggestion]);
+
+  console.log("movies: ", movies);
+  console.log("genre: ", genre);
+  console.log("suggestionData: ", suggestionData);
+  console.log("suggestion: ", suggestion);
+  console.log("error: ", error);
+
+  if (error) {
+    return <div>Error: {error}</div>;
   }
+
+  if (!suggestionData) {
+    return <div>Loading...</div>;
+  }
+
+
+  const { Title, Genre, Plot, Runtime, imdbRating, Released, Poster } = suggestionData.movieData;
+
 
 
   return (
     <div>
-      
+      <img src={Poster} alt={Title} />
+      <p>Title: {Title}</p>
+      <p>Genre: {Genre}</p>
+      <p>Plot: {Plot}</p>
+      <p>Runtime: {Runtime}</p>
+      <p>IMDB Rating: {imdbRating}</p>
+      <p>Released: {Released}</p>
     </div>
   )
 }
--- src/pages/Chance/Chance.jsx
+++ src/pages/Chance/Chance.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./Chance.scss";
+import ChanceLogic from '../../Components/ChanceComp/ChanceLogic/ChanceLogic';
 
 const Chance = () => {
   return (
     <div>
-      
+      <ChanceLogic />
     </div>
   )
 }
